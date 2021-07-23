b82e9a59b400a88bbbc8498a38986103db72704f
Eli Anderson <dispentia@gmail.com>
2021-07-23 15:25:18 -0700

    init commit

---
+++ src/client/views/home.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template, session
+
+home_bp = Blueprint('home', __name__, template_folder='templates')
+
+
+@home_bp.route('/')
+def home():
+    """ REST endpoint for the home page. """
+    return render_template('home.html', title='Home')
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from client.app import app
+
+
+def main():
+    """ Runs the application. """
+    app.run()
+
+
+if __name__ == '__main__':
+    main()

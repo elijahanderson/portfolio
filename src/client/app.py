a63c5ecc9f44009583f341f33dc84e81cd37de3c
Eli <dispentia@gmail.com>
2021-07-28 01:15:02 +0000

    master: implement about and contact page

--- src/client/app.py
+++ src/client/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, session
 
 from client.views.home import home_bp
-#from client.views.about import about_bp
+from client.views.about import about_bp
 #from client.views.work import work_bp
 #from client.views.blog import blog_bp
-#from client.views.contact import contact_bp
+from client.views.contact import contact_bp
 
 """ Initialize the app. """
 app = Flask(__name__)
 app.register_blueprint(home_bp)
-#app.register_blueprint(about_bp)
+app.register_blueprint(about_bp)
 #app.register_blueprint(work_bp)
 #app.register_blueprint(blog_bp)
-#app.register_blueprint(contact_bp)
+app.register_blueprint(contact_bp)
 app.secret_key = "secret key"
